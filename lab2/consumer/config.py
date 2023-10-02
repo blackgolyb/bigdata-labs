19f50f37c365f0f1bc73ee3afad955325372fb15
Blackgolyb <andrejomelnickij@gmail.com>
2023-10-03 02:11:53 +0300

    add pydantic for orders and add ranking of orders

--- lab2/consumer/config.py
+++ lab2/consumer/config.py
@@ -1,2 +1,3 @@
 KAFKA_HOST = "localhost"
 KAFKA_PORT = 29092
+KAFKA_TOPIC = "bitstamp"
--- lab2/consumer/main.py
+++ lab2/consumer/main.py
@@ -1,39 +1,89 @@
 import json
 
+from pydantic import BaseModel
 from kafka import KafkaConsumer
 
 from config import *
 
 
-def main():
-    consumer = KafkaConsumer(
-        "bitstamp",
-        bootstrap_servers=f"{KAFKA_HOST}:{KAFKA_PORT}",
-        value_deserializer=lambda m: json.loads(m.decode("ascii")),
-    )
+class OrderData(BaseModel):
+    id: int
+    id_str: str
+    order_type: int
+    datetime: str
+    microtimestamp: str
+    amount: float
+    amount_str: str
+    amount_traded: str
+    amount_at_create: str
+    price: int
+    price_str: str
 
-    for msg in consumer:
-        print(msg.value)
+
+class Order(BaseModel):
+    data: OrderData
+    channel: str
+    event: str
+
+    def get_price(self):
+        return self.data.price
+
+    def __lt__(self, other):
+        return self.data.price < other.data.price
+
+    def __le__(self, other):
+        return self.data.price <= other.data.price
+
+    def __eq__(self, other):
+        return self.data.price == other.data.price
+
+    def __ne__(self, other):
+        return self.data.price != other.data.price
+
+    def __gt__(self, other):
+        return self.data.price > other.data.price
+
+    def __ge__(self, other):
+        return self.data.price >= other.data.price
+
+
+class App(object):
+    def __init__(self, kafka_urls: str | list[str], topic: str):
+        self.best_orders = dict()
+        self.configure_kafka(kafka_urls, topic)
+
+    def configure_kafka(self, kafka_urls: str | list[str], topic: str):
+        self.consumer = KafkaConsumer(
+            topic,
+            bootstrap_servers=kafka_urls,
+            value_deserializer=lambda m: json.loads(m.decode("ascii")),
+        )
+
+    def process_message(self, message: dict):
+        order = Order.parse_obj(message)
+
+        if order.event not in self.best_orders:
+            self.best_orders[order.event] = list()
+            self.best_orders[order.event].append(order)
+            return
+
+        if order > min(self.best_orders[order.event]):
+            if len(self.best_orders[order.event]) >= 10:
+                self.best_orders[order.event].pop()
+
+            self.best_orders[order.event].append(order)
+
+        self.best_orders[order.event].sort(reverse=True)
+
+    def start(self):
+        for message in self.consumer:
+            self.process_message(message.value)
+
+
+def main():
+    app = App(topic=KAFKA_TOPIC, kafka_urls=f"{KAFKA_HOST}:{KAFKA_PORT}")
+    app.start()
 
 
 if __name__ == "__main__":
     main()
-
-
-# {
-#     "data": {
-#         "id": 1668902609674242,
-#         "id_str": "1668902609674242",
-#         "order_type": 1,
-#         "datetime": "1696281903",
-#         "microtimestamp": "1696281903146000",
-#         "amount": 1.07512726,
-#         "amount_str": "1.07512726",
-#         "amount_traded": "0.00184000",
-#         "amount_at_create": "1.07696726",
-#         "price": 27856,
-#         "price_str": "27856",
-#     },
-#     "channel": "live_orders_btcusd",
-#     "event": "order_changed",
-# }
