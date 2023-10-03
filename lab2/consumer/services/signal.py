8d56143d506cb58a60e7686f0b25b01ecf4010c4
Blackgolyb <andrejomelnickij@gmail.com>
2023-10-03 13:37:22 +0300

    added services and utils

---
+++ lab2/consumer/services/signal.py
@@ -0,0 +1,38 @@
+class Signal:
+    def __init__(self):
+        self.clear()
+
+    def connect(self, callback):
+        if callback not in self._callbacks:
+            self._callbacks.append(callback)
+
+    def disconnect(self, callback):
+        if callback in self._callbacks:
+            self._callbacks.remove(callback)
+
+    def clear(self):
+        self._callbacks: list = []
+
+    def emit(self, *args, **kwargs):
+        for callback in self._callbacks:
+            callback(*args, **kwargs)
+
+
+class AsyncSignal:
+    def __init__(self):
+        self.clear()
+
+    def connect(self, callback):
+        if callback not in self._callbacks:
+            self._callbacks.append(callback)
+
+    def disconnect(self, callback):
+        if callback in self._callbacks:
+            self._callbacks.remove(callback)
+
+    def clear(self):
+        self._callbacks: list = []
+
+    async def emit(self, *args, **kwargs):
+        for callback in self._callbacks:
+            await callback(*args, **kwargs)
